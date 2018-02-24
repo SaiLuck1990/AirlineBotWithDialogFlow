fb4fe131278c4e998d579625b938ad749824219b
jananiiyer <sailakshmikumaar@gmail.com>
2018-02-24 10:56:26 +0530

    SS : Added logic for retrieving airports

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/airports',function(req,res){
               }
             res.json({
             //speech: responseJson,
-            displayText: JSON.stringify(replies),
+            //displayText: JSON.stringify(replies),
             messages:[
                    {
                     title:"Please select an airport in "+city,
