430f6bc044f6e218d337a75237c45e41307506e8
jananiiyer <sailakshmikumaar@gmail.com>
2018-02-24 23:47:08 +0530

    SS : Added logic for flight search

--- index.js
+++ index.js
@@ -16,21 +16,124 @@ app.get('/hello' ,function(req,res){
 });
 
 app.post('/airports',function(req,res){
-    console.log("Your request is"+JSON.stringify(req.body));
+    //console.log("Your request is"+JSON.stringify(req.body));
     var city="";
     var source = req.body.result.source;
     if(req.body.result.action==="findestinationairport"){
        city=req.body.result.parameters.destination.city;
-       sendResponse(req,res,city,source);
+       sendAirports(req,res,city,source);
     } else if(req.body.result.action==="findoriginairport"){
       city=req.body.result.parameters.origin.city;
-       sendResponse(req,res,city,source);
+       sendAirports(req,res,city,source);
     } else if(req.body.result.action ==="triptype"){
         sendTripTypes(req,res,source);
+    }else if(req.body.result.action === "flightsearch"){
+        searchFlights(req,res,source);
     }
 });
-function sendTripTypes(req,res,source){
 
+
+function searchFlights(req,res,source){
+var depDate = req.body.result.parameters.date;
+var cabin = req.body.result.parameters.cabin;
+var adultCount = 0;
+var youthCount = 0;
+var childCount = 0;
+var infantCount = 0;
+ var replies = [];
+if(null !== req.body.result.parameters.adultcount){
+  adultCount = req.body.result.parameters.adultCount;
+}else if (null != req.body.result.parameters.childcount){
+    childCount = req.body.result.parameters.childcount;
+}else if(null != req.body.result.parameters.infantcount){
+    infantCount = req.body.result.parameters.infantcount;
+}else if(null != req.body.result.parameters.youthcount){
+    youthCount = req.body.result.parameters.youthcount;
+}
+var destination = req.body.result.parameters.toairport.IATA;
+var origin = req.body.result.parameters.fromairport.IATA;
+
+console.log("Destination is"+destination);
+console.log("Origin is"+origin);
+
+     request({
+         url: "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search",
+         qs: {
+                    apikey: "J5ZkcObTCaZvq9MlYs0rjycwAx8PdVTk",
+                    origin: origin,
+                    destination : destination,
+                    departure_date : depDate,
+                    adults : adultCount,
+                    children: childCount,
+                    infants : infantCount,
+                    include_airlines : "BA",
+                    nonstop : "false",
+                    travel_class : cabin,
+                    number_of_results : 10
+                },
+         method: 'GET',
+        json: true
+    },function(error, response, body){ 
+        console.log(JSON.stringify(body));
+    if (!error && response.statusCode == 200) { 
+        var replies = getTotalListOfFare(body);
+         if(source === "agent"){
+                messages = [
+                   {
+                    title:"Please select one of the fares for your journey",
+                    replies:replies,
+                    type: 2}
+                ];
+            } else if (source === "facebook"){
+                messages = [
+                   {
+                    title:"Please select one of the fares for your journey",
+                    platform:"facebook",
+                    replies:replies,
+                    type: 2}
+                ];
+            }
+         res.json({
+            //speech: responseJson,
+            //displayText: "This is your display text",
+            messages:messages,
+            source: source
+           });  
+		  }else{
+			  res.json({
+                  displayText:response.body.message,
+                  type: 0});
+		  }
+    });
+}
+
+function getTotalListOfFare(body){
+  for(var i=0;i<body.results.length;i++){
+            var adultFare = 0;
+            var childFare=0;
+            var infantFare=0;
+            var total=0;
+            var replies =[];
+            if(null != body.results[i].fare.price_per_adult){
+              adultFare = body.results[i].fare.price_per_adult.total_fare;
+            }
+            if(null!=body.results[i].fare.price_per_child){
+                childFare = body.results[i].fare.price_per_child.total_fare;
+            }
+            if(null!=body.results[i].fare.price_per_infant){
+                infantFare = body.results[i].fare.price_per_infant.total_fare;
+            }
+
+            total = adultFare + childFare + infantFare;
+            console.log("totalFare"+total);
+            //replies.push[total];
+            replies.push[total];
+            console.log("replies"+replies.length);
+        }
+        return replies;
+}
+
+function sendTripTypes(req,res,source){
     messages = [
                    {
                     title:"Please select a trip type",
@@ -44,7 +147,9 @@ function sendTripTypes(req,res,source){
             messages:messages,
             source: source});
 }
- function sendResponse(req,res,city,source){
+
+
+ function sendAirports(req,res,city,source){
     var outString = "Result is";
     console.log("City sent from Bot is"+city);
     request({
