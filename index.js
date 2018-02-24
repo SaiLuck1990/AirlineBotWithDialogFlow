23fb3f025babb339a52642bc96977b35210c2258
jananiiyer <sailakshmikumaar@gmail.com>
2018-02-24 12:58:15 +0530

    SS : Added logic for retrieving airports

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/hello' ,function(req,res){
 });
 
 app.post('/airports',function(req,res){
-    console.log("Your request is"+JSON.stringify(req));
+    console.log("Your request is"+JSON.stringify(req.body));
     var city="";
     if(req.body.result.action==="findestinationairport"){
        city=req.body.result.parameters.destination.city;
